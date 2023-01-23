729f492f6ca539e0c96fd4b9e3e18a1813dae529
Dave Estey <daveestey.working@gmail.com>
2023-01-23 02:46:40 -0400

    Added route files, server file, and the helper page, as well as made the post and get methods for the database, added all the routes for the server and the index to read from the database

---
+++ routes/notes.js
@@ -0,0 +1,26 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+
+notes.get("/", (req, res) => readFromFile("./db/db.json")
+  .then((data) => res.json(JSON.parse(data)))
+);
+
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+        const newNote = { title, text, uuid: uuidv4() };
+
+  readAndAppend(newNote, "./db/db.json");
+  res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+module.exports = notes;
+
+
+
+
